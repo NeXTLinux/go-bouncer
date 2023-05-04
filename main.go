03a5bfdcefa6c58a94a3c522b50d87a179afe36d
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-05-03 20:16:03 -0700

    Create main.go
    Signed-off-by: Cyber OS Lab. <99838653+cyberoslab@users.noreply.github.com>

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/nextlinux/go-bouncer/cmd"
+
+var (
+	version   = "No version provided"
+	commit    = "No commit provided"
+	buildTime = "No build timestamp provided"
+)
+
+func main() {
+	cmd.SetVersion(&cmd.Version{
+		Version:   version,
+		Commit:    commit,
+		BuildTime: buildTime,
+	})
+
+	cmd.Execute()
+}
